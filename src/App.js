818bafbe46447a4cbc358356771c94b7be9bf60c
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-03-20 11:35:12 +0100

    final css changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <TextField />
       <Switch />
       <Checkboxes />
+      <RadioButtons />
     </div>
   );
 }
--- src/Components/RadioButtons.js
+++ src/Components/RadioButtons.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import "../Css/RadioButtons.css"
+
+
 
 export class RadioButtons extends Component {
     render() {
         return (
-            <div>
-                
+            <>
+            <div className="firstRadio">
+                <label class="radio-box">
+                    <input type="radio" name="radio" className="Radio" />
+                    <span class="radio-checkmark"></span>
+                </label>
             </div>
+
+            <div className="secondRadio">
+                <label class="radio-box">
+                    <input type="radio" name="radio" className="Radio" />
+                    <span class="radio-checkmark"></span>
+                </label>
+            </div>
+           
+            </>
         )
     }
 }
